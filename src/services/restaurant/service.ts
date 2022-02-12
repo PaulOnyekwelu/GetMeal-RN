44ce641b1ef9f1ea87d78bee427bb1102973d9a1
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-02-12 18:31:48 +0100

    code refactoring

--- src/services/restaurant/service.ts
+++ src/services/restaurant/service.ts
@@ -1,6 +1,6 @@
 import { mocks } from "./mock";
 
-export const restaurantService = (location) => {
+export const restaurantService = (location: string): Promise<any> => {
   return new Promise((resolve, reject) => {
     const restaurants = mocks[location];
     if (!restaurants) reject("Restaurants not found");
